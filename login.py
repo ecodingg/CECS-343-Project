cad870fa401e18959873ee4b8f262f5ea5112d50
E <ecoding@pm.me>
2023-04-12 22:56:01 -0700

    Menu part 1

--- login.py
+++ login.py
@@ -28,7 +28,61 @@ def loginInput():
    return loginText, passwordText
    top.mainloop()
 
+<<<<<<< Updated upstream
 def loginValidation():
    #Checks that the input for Login is Valid
    print("This is not done yet")
+=======
+   username = loginButton
+   password = passwordButton
+
+   test = loginCredentials()
+
+   if (test):
+      # credentials = LoginInfo()
+      # credentials.username = username
+      # credentials.password = password
+   
+      f = open("loginInfo.txt", "w")
+      f.write(username)
+      f.write("\n")
+      f.write(password)
+      f.close()
+   
+   else:
+      testTwo = loginValidation(username, password)
+      if (testTwo):
+         menu()
+      else:
+         print("Try to login again")
+
+   top.mainloop()
+   
+
+   
+   
+#Checks to see that Login works
+def loginValidation(user, passW):
+   f = open("loginInfo.txt", "w")
+   loginArray = []
+   
+   for i in range(1,3):
+      loginArray.append(f.readline())
+
+   if (user == loginArray[1]):
+      if(passW == loginArray[2]):
+         return True
+   else:
+      print("Incorrect Login Credentials")
+      return False
+
+
+def loginCredentials():
+   f = open("loginInfo.txt", "w")
+   if (len(f.read()) == 0):
+      return True
+   else:
+      return False
+   
+>>>>>>> Stashed changes
    
--- menu.py
+++ menu.py
@@ -1,22 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
   
   
 def menu():
     root = tk.Tk()
-    root.title("Tab Widget")
-    tabControl = ttk.Notebook(root)
-    
-    tab1 = ttk.Frame(tabControl)
-    tab2 = ttk.Frame(tabControl)
-    tab3 = ttk.Frame(tabControl)
-    tab4 = ttk.Frame(tabControl)
-    
-    tabControl.add(tab1, text ='Tenant List')
-    tabControl.add(tab2, text ='Rental Income Record')
-    tabControl.add(tab3, text='Expense Record')
-    tabControl.add(tab4, text='Annual Summary')
-    tabControl.pack(expand = 1, fill ="both")
+
 
     root.mainloop()
-    
+
+def exepnseButton():
+    print("Not done")
+
+def annualButton():
+    print("Not done")
+
+def tenantButton():
+    print("Not done")
