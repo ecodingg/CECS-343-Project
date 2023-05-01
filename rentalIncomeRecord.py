5d844b8ce28edc38a2decdc8a4003016b78e6f3f
E <ecoding@pm.me>
2023-05-01 12:10:20 -0700

    Update Rental Income Record

--- rentalIncomeRecord.py
+++ rentalIncomeRecord.py
@@ -1,27 +1,34 @@
-
 from tkinter import *
 from tenant import Tenant
+import csv
+tenants = []
 
-"""INITIALIZED SOME OF TENANTS TO PRACTICE MAKING CHART. FOR THIS TO WORK, 
-        def __init__(self,first,last,email,age,rent,apt):
-        self.firstName = first
-        self.lastName = last
-        self.email = email
-        self.age = age
-        self.rent = rent
-        self.apt = apt
-    must be added to tenant class
-"""
-kevin = Tenant("kevin","lee","Example@gmail.com",24,1400,1)
-sarah = Tenant("sarah","chang","sc@gmail.com",22,1200,2)
-shaco = Tenant("shaco","bao","clown@gmail.com",24,1600,3)
-garen = Tenant("garen","demacia","gdemacia@gmail.com",25,1400,4)
-lux = Tenant("lux","light","lightwillguide@gmail.com",28,1200,5)
-practice = [kevin,sarah,shaco,garen,lux]
+def removeTenant(email):
+    for ten in tenants:
+        if(ten.email == email):
+            tenants.remove(ten)
+def removeRow(email):
+    removeTenant(email)
+    for label in window.winfo_children():
+        label.destroy()
+    startWindow()
+
+def readData():
+    f = open("tenantTest.txt","r")
+    for x in f:
+        split = x.split(",")
+        resident = Tenant(split[0],split[1],split[2],split[3],split[4],split[5],split[6],split[7],split[8])
+        tenants.append(resident)
+    f.close()
+def saveData():
+    f = open("tenantTest.txt","w")
+    for tenant in tenants:
+        f.write(tenant.firstName + "," + tenant.lastName + "," + tenant.email + "," + tenant.age + ",")
+        f.write(tenant.rent + "," + tenant.apt + "," + tenant.payDue + "," + tenant.paidDate + "," + tenant.onTime )
+    f.close()
 
 def writeTenants(): #reads TenantLists and make chart in window
-    print("READ FROM THE LIST AND ADD THEM")
-    tenants = practice #IN THE FUTURE NEED FUNCTION TO BRING LIST OF TENANTS FROM STORED DATA
+    # print("READ FROM THE LIST AND ADD THEM")
     tRow = 2
     for ten in range(len(tenants)):
         tenant = tenants[ten]
@@ -29,17 +36,66 @@ def writeTenants(): #reads TenantLists and make chart in window
         tName.grid(row = tRow, column = 0)
         tRent = Label(window, text=tenant.rent, font=("Arial", 14), padx=10, pady=10)
         tRent.grid(row = tRow, column=1)
-        tPDD = Label(window, text = "WORKLATER", font=("Arial", 14), padx=10, pady=10)
+        tPDD = Label(window, text = tenant.paymentDueDate, font=("Arial", 14), padx=10, pady=10)
         tPDD.grid(row=tRow, column=2)
-        tPD = Label(window, text = "PAYY DATE" , font=("Arial", 14), padx=10, pady=10)
+        tPD = Label(window, text = tenant.paymentDate , font=("Arial", 14), padx=10, pady=10)
         tPD.grid(row = tRow, column=3)
-        tOnTime = Label(window, text = "ONTIME?", font=("Arial", 14), padx=10, pady=10)
+        tOnTime = Label(window, text = tenant.isPaid, font=("Arial", 14), padx=10, pady=10)
         tOnTime.grid(row=tRow, column=4)
+        delete = Button(window, text = 'X', command=lambda email = tenant.email: removeRow(email))
+        delete.grid(row = tRow, column = 5)
         tRow = tRow +1
 
 def editRecord():
-    print("WORK")
+    tRow = 2
+    for ten in range(len(tenants)):
+        tenant = tenants[ten]
+        tName = Entry(window)
+        tName.insert(0, tenant.firstName + " " + tenant.lastName)
+        tName.grid(row = tRow, column = 0)
+        tRent = Entry(window)
+        tRent.insert(0, tenant.rent)
+        tRent.grid(row = tRow, column=1)
+        tPDD = Entry(window)
+        tPDD.insert(0, tenant.paymentDueDate)
+        tPDD.grid(row=tRow, column=2)
+        tPD = Entry(window)
+        tPD.insert(0,tenant.paymentDate)
+        tPD.grid(row = tRow, column=3)
+        tOnTime = Entry(window)
+        tOnTime.insert(0,tenant.isPaid)
+        tOnTime.grid(row=tRow, column=4)
 
+        tRow = tRow +1
+
+def save():
+    counter = 0
+    for label in window.winfo_children():
+        if(type(label) == Entry):
+
+            person = counter//5
+            section = counter%5
+            if (section ==0 ):
+                firstName = label.get().split()[0]
+                lastName = label.get().split()[1]
+                # print("First: "+ firstName)
+                # print( label.get().split())
+                # print("LAst: " + lastName)
+                tenants[person].firstName = firstName
+                tenants[person].lastName = lastName
+            elif (section ==1):
+                tenants[person].rent = label.get()
+            elif (section ==2):
+                tenants[person].payDue = label.get()
+            elif (section ==3):
+                tenants[person].paidDate = label.get()
+            elif (section ==4):
+                tenants[person].onTime = label.get()
+
+            counter = counter +1
+    for label in window.winfo_children():
+        label.destroy()
+    startWindow()
 def addTenantRow():
     global tenantRow
     newFirstName = Entry(window)
@@ -58,8 +114,6 @@ def addTenantRow():
 
     tenantRow += 1
 
-def submitChange():
-    print("WORK")
 
 window = Tk()
 def startWindow():
@@ -80,9 +134,17 @@ def startWindow():
 
     editButton = Button(window, text="Edit", command=editRecord)
     editButton.grid(row=100, column=0, columnspan=2, sticky="n")
-    submitButton = Button(window, text="Remove Tenant", command= submitChange)
-    submitButton.grid(row=100, column=3, columnspan=2, sticky="n")
+    saveButton = Button(window, text="Save", command=save)
+    saveButton.grid(row=100, column=4, columnspan=2, sticky="n")
+    saveFile = Button(window, text="Save To File", command = saveData)
+    saveFile.grid(row=100, column =2, columnspan =2,  sticky = "n")
 
     writeTenants()
     window.mainloop()
-startWindow()
+
+def main():
+    readData()
+    startWindow()
+
+if __name__ == "__main__":
+    main()
